c163b15f2322e9703af41e9f3c7b4ab323557c85
Prateek Machiraju <prateek.machiraju@gmail.com>
2019-09-07 18:49:33 -0400

    Finish NEO code

--- src/main/kotlin/org/ghrobotics/frc2019/subsystems/Drivetrain.kt
+++ src/main/kotlin/org/ghrobotics/frc2019/subsystems/Drivetrain.kt
@@ -8,8 +8,6 @@
 
 package org.ghrobotics.frc2019.subsystems
 
-import com.ctre.phoenix.sensors.PigeonIMU
-import com.revrobotics.CANSparkMax
 import com.revrobotics.CANSparkMaxLowLevel
 import edu.wpi.first.wpilibj.geometry.Rotation2d
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics
@@ -18,64 +16,89 @@ import org.ghrobotics.frc2019.Constants
 import org.ghrobotics.frc2019.commands.TeleopDriveCommand
 import org.ghrobotics.lib.mathematics.twodim.control.RamseteTracker
 import org.ghrobotics.lib.mathematics.units.Meter
-import org.ghrobotics.lib.mathematics.units.inch
-import org.ghrobotics.lib.mathematics.units.nativeunit.NativeUnitLengthModel
-import org.ghrobotics.lib.mathematics.units.nativeunit.nativeUnits
-import org.ghrobotics.lib.motors.FalconMotor
+import org.ghrobotics.lib.mathematics.units.SIUnit
+import org.ghrobotics.lib.mathematics.units.derived.volts
 import org.ghrobotics.lib.motors.rev.FalconMAX
 import org.ghrobotics.lib.physics.MotorCharacterization
 import org.ghrobotics.lib.subsystems.drive.FalconWestCoastDrivetrain
 import org.ghrobotics.lib.utils.Source
-import org.ghrobotics.lib.utils.asSource
 
 object Drivetrain : FalconWestCoastDrivetrain() {
 
-    // Constants are at the top of each subsystem.
-    // These must be private.
-
-    private const val kPigeonId = 17
-
-    private const val kBeta = 2.0
-    private const val kZeta = 0.7
-
     // Private member variables
-    private val pigeon = PigeonIMU(kPigeonId)
+    // private val pigeon = PigeonIMU(kPigeonId)
 
     // Overriden variables
 
     // Motors
-    override val leftMotor = FalconMAX(Constants.kLeftMasterId, CANSparkMaxLowLevel.MotorType.kBrushless, Constants.kDriveNativeUnitModel)
-    override val rightMotor = FalconMAX(Constants.kRightMasterId, CANSparkMaxLowLevel.MotorType.kBrushless, Constants.kDriveNativeUnitModel)
-    private val leftSlave1 = FalconMAX(Constants.kLeftSlave1Id, CANSparkMaxLowLevel.MotorType.kBrushless, Constants.kDriveNativeUnitModel)
-    private val rightSlave1 = FalconMAX(Constants.kRightSlave1Id, CANSparkMaxLowLevel.MotorType.kBrushless, Constants.kDriveNativeUnitModel)
+    override val leftMotor =
+        FalconMAX(
+            Constants.Drivetrain.kLeftMasterId,
+            CANSparkMaxLowLevel.MotorType.kBrushless,
+            Constants.Drivetrain.kNativeUnitModel
+        )
+    override val rightMotor =
+        FalconMAX(
+            Constants.Drivetrain.kRightMasterId,
+            CANSparkMaxLowLevel.MotorType.kBrushless,
+            Constants.Drivetrain.kNativeUnitModel
+        )
+    private val leftSlave1 =
+        FalconMAX(
+            Constants.Drivetrain.kLeftSlave1Id,
+            CANSparkMaxLowLevel.MotorType.kBrushless,
+            Constants.Drivetrain.kNativeUnitModel
+        )
+    private val rightSlave1 =
+        FalconMAX(
+            Constants.Drivetrain.kRightSlave1Id,
+            CANSparkMaxLowLevel.MotorType.kBrushless,
+            Constants.Drivetrain.kNativeUnitModel
+        )
 
     // Motor characterization
-    override val leftCharacterization: MotorCharacterization<Meter> = TODO()
-    override val rightCharacterization: MotorCharacterization<Meter> = TODO()
+    override val leftCharacterization = MotorCharacterization<Meter>(SIUnit(0.0), SIUnit(0.0), SIUnit(0.0))
+    override val rightCharacterization = MotorCharacterization<Meter>(SIUnit(0.0), SIUnit(0.0), SIUnit(0.0))
 
     // Gyro
-    override val gyro: Source<Rotation2d> = pigeon.asSource()
+    override val gyro: Source<Rotation2d> = { Rotation2d() }
 
     // Kinematics
-    override val kinematics: DifferentialDriveKinematics = TODO()
+    override val kinematics = DifferentialDriveKinematics(Constants.Drivetrain.kTrackWidth.value)
 
     // Odometry
     override val odometry = DifferentialDriveOdometry(kinematics)
 
     // Trajectory tracker
-    override val trajectoryTracker = RamseteTracker(kBeta, kZeta)
+    override val trajectoryTracker = RamseteTracker(Constants.Drivetrain.kBeta, Constants.Drivetrain.kZeta)
 
     // Constructor
     init {
+        leftMotor.outputInverted = false
+        rightMotor.outputInverted = true
+
+        leftSlave1.follow(leftMotor)
+        rightSlave1.follow(rightMotor)
+
+        listOf(leftMotor, rightMotor).forEach { motor ->
+            motor.brakeMode = true
+            motor.voltageCompSaturation = 12.volts
+            motor.canSparkMax.setSmartCurrentLimit(38) // Amperes
+        }
+
         defaultCommand = TeleopDriveCommand()
     }
 
-    // Emergency management.
+    // Emergency management
     override fun activateEmergency() {
-        TODO()
+        listOf(leftMotor, rightMotor).forEach { motor ->
+            motor.controller.p = 0.0
+        }
     }
 
     override fun recoverFromEmergency() {
-        TODO()
+        listOf(leftMotor, rightMotor).forEach { motor ->
+            motor.controller.p = Constants.Drivetrain.kP
+        }
     }
 }
