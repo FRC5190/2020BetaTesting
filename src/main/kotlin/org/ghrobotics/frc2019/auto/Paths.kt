af533668c779aa3109eece477fe4b5c6cfd4a7e4
Prateek Machiraju <prateek.machiraju@gmail.com>
2019-11-03 10:45:14 -0500

    Update to 2020 Beta 2 (#13)

--- src/main/kotlin/org/ghrobotics/frc2019/auto/Paths.kt
+++ src/main/kotlin/org/ghrobotics/frc2019/auto/Paths.kt
@@ -11,28 +11,26 @@ package org.ghrobotics.frc2019.auto
 import edu.wpi.first.wpilibj.geometry.Rotation2d
 import edu.wpi.first.wpilibj.trajectory.Trajectory
 import edu.wpi.first.wpilibj.trajectory.constraint.CentripetalAccelerationConstraint
+import edu.wpi.first.wpilibj.util.Units
 import org.ghrobotics.lib.mathematics.twodim.geometry.Pose2d
+import org.ghrobotics.lib.mathematics.twodim.trajectory.FalconTrajectoryConfig
 import org.ghrobotics.lib.mathematics.twodim.trajectory.FalconTrajectoryGenerator
 import org.ghrobotics.lib.mathematics.units.derived.acceleration
 import org.ghrobotics.lib.mathematics.units.derived.velocity
 import org.ghrobotics.lib.mathematics.units.feet
-import org.ghrobotics.lib.mathematics.units.meters
 
 /**
  * Container for autonomous paths.
  */
 object Paths {
+    val trajectoryConfig = FalconTrajectoryConfig(8.feet.velocity, 3.feet.acceleration)
+        .addConstraint(CentripetalAccelerationConstraint(Units.feetToMeters(6.0)))
     val trajectory: Trajectory = FalconTrajectoryGenerator.generateTrajectory(
         listOf(
             Pose2d(5.326.feet, 9.697.feet, Rotation2d.fromDegrees(0.0)),
             Pose2d(18.651.feet, 6.997.feet, Rotation2d.fromDegrees(-23.0)),
             Pose2d(22.13.feet, 2.612.feet, Rotation2d.fromDegrees(-148.0))
         ),
-        listOf(CentripetalAccelerationConstraint(2.1336)),
-        0.0.meters.velocity,
-        0.0.meters.velocity,
-        8.feet.velocity,
-        3.0.feet.acceleration,
-        false
+        trajectoryConfig
     )
 }
